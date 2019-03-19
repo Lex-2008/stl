a512af6b64e0dbe7082a4505c30106441c125bca
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-03-19 18:49:13 +0100

    Add the code

---
+++ stl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if test "$#" != "3"; then
+	echo "$0 <template> <list> <pages>"
+	exit 1
+fi
+
+TEMPLATE="$1"
+LIST="$2"
+PAGES="$3"
+NL="
+"
+
+
+while read name tags; do
+	echo "building [$name.html]..."
+	tags1="$( echo "$name" | tr '-' '\n' )"
+	tags2="$( echo "$tags" | tr ' ' '\n' )"
+	tags="$( echo "all$NL$name$NL$tags1$NL$tags2" | sort -u | sed '/^$/d')"
+	sed="$(echo "$tags" | sed 's_.*_/<!-- begin & -->/,/<!-- end & -->/p;/<!-- for & -->/p_')"
+	test -f "$PAGES/$name.html" && sed="$sed;$NL/<!-- main content -->/r $PAGES/$name.html"
+	sed -n "$sed" "$TEMPLATE" >"$name.html"
+done <"$LIST"
