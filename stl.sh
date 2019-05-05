8696af446cf197aa805e251e3051bbbb6b0efbe0
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-05-05 23:12:37 +0200

    Work with files in dirs, too
    Issue was that sed expression was broken when it contained filename with
    forward slash in it. We replace it with dash for tags.

--- stl.sh
+++ stl.sh
@@ -14,9 +14,10 @@ NL="
 
 while read name tags; do
 	echo "building [$name.html]..."
-	tags1="$( echo "$name" | tr '-' '\n' )"
+	tags1="$( echo "$name" | tr '/-' '\n' )"
 	tags2="$( echo "$tags" | tr ' ' '\n' )"
-	tags="$( echo "all$NL$name$NL$tags1$NL$tags2" | sort -u | sed '/^$/d')"
+	sedname="$( echo "$name" | tr '/' '-' )"
+	tags="$( echo "all$NL$sedname$NL$tags1$NL$tags2" | sort -u | sed '/^$/d')"
 	sed="$(echo "$tags" | sed 's_.*_/<!-- begin & -->/,/<!-- end & -->/p;/<!-- for & -->/p_')"
 	test -f "$PAGES/$name.html" && sed="$sed;$NL/<!-- main content -->/r $PAGES/$name.html"
 	sed -n "$sed" "$TEMPLATE" >"$name.html"
